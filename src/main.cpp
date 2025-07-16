61ac9833faf7c5c8f92e623152a533f61dcddd76
Franco Bustios <166667809+fbustios@users.noreply.github.com>
2025-07-16 12:01:29 -0600

    window

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,22 @@
 using namespace std;
 
 int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800,800,"window", NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
